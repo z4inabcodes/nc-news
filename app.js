f8c163fede3c848ea965203f578f299ac013fda5
zainab ali <zainab.ali056@outlook.com>
2025-03-11 12:27:34 +0000

    completed task 3

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ const app = express()
 const {getEndpoint} =require('./db/controllers/getEnpoint.controllers')
 const {getTopics} =require('./db/controllers/topics.controller')
 const {handleWrongEndPoints}=require('./db/controllers/errors.contollers')
-// const {getArticlesById} =require('./db/controllers/articles.controllers')
+const {getArticlesById} =require('./db/controllers/articles.controllers')
+
 app.get('/api', getEndpoint)
 app.get('/api/topics',getTopics)
-// app.get('/api/articles/:article_id',getArticlesById)
+ app.get('/api/articles/:article_id',getArticlesById)
 
 app.all('/*', handleWrongEndPoints)
+app.use((err,req,res,next)=> {
+    if (err.status) {
+        res.status(err.status).send({msg:err.msg });
+    } else{
+        res.status(500).send({ msg:'Internal Server Error'});
+    }
+});
 
 module.exports=app
--- db/models/articles.model.js
+++ db/models/articles.model.js
@@ -1,1 +1,9 @@
 const db = require('../connection')
+exports.fetchArticlesById = (article_id) => {
+    return db.query('SELECT * FROM articles WHERE article_id= $1',[article_id]).then(({rows})=>{
+        if (rows.length ===0){
+            return Promise.reject({status:404,msg:'Invalid id!'})
+        }
+    return rows[0]
+    })
+};
